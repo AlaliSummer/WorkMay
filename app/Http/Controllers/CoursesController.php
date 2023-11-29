30f3097909bebc0051342531822a4474c8cdba33
AlaliSummer <samar.h@ptc-ksa.net>
2023-11-29 10:30:51 +0300

    Display Courses Index

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class CoursesController extends Controller
 {
     public function Index() {
-        return Inertia::render('Courses/Index');
+        return Inertia::render('Courses/Index',
+            ['courses' => Course::latest()->paginate(10),]);
     }
 }
--- resources/js/Components/CourseCard.vue
+++ resources/js/Components/CourseCard.vue
@@ -3,7 +3,7 @@
         <div class="d-flex">
             <img src="https://piktochart.com/wp-content/uploads/2023/07/large-92-600x848.jpg"><br/>
         </div>
-        <p>{{ courses.title }}</p>
+        <p>{{ course.title }}</p>
 <!--        <p>courses.title</p>-->
         <button @click="open" class="items-center px-4 py-2 bg-gray-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase ltr:tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
             {{ $t('words.course-details') }}
@@ -18,6 +18,7 @@ export default {
     components: {
     },
     props: ['users', 'courses'],
+    name: "CourseCard",
     data() {
         return {
         }
