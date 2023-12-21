30a244a3adc2a895fe7d311455e3668b79f0c7f2
AlaliSummer <samar.h@ptc-ksa.net>
2023-12-21 15:32:32 +0300

    build

--- public/build/assets/WishList-5de486cb.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{e as o,o as r}from"./app-8dd44ad4.js";const t={};function c(s,n){return r(),o("p")}const f=e(t,[["render",c]]);export{f as default};
